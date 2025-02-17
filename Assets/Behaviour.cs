5e4136caa74ee80d28347d72e24b281d154c7a49
Ben Harris (s5721164) <s5721164@bournemouth.ac.uk>
2025-02-17 13:31:04 +0000

    Main Game Publish
    yesss

---
+++ Assets/Behaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Behaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    private int Angle = 45;
+    public int Damp = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+         
+    }
+}
